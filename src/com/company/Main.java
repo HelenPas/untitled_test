2ce9288cb587e596d554edabbde39337076e9f7c
ZhQa <epastushenko88@gmail.com>
2020-01-11 17:38:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public void  sum(int a, int b) {
+     int c = a + b;
+     System.out.println(c);
+    }
+    public int sum(int a, int b, int c){
+        int d = a + b + c;
+        return d;
+    }
+    public void sum(double a,)
+}
+
